325b5896b5851a7351b7c9f40e50c3f42437e9fe
Dr O <droctgone@gmail.com>
2021-06-15 15:22:42 -0700

    template start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
+import { Canvas } from "react-three-fiber"
+import { useSpring, a } from 'react-spring/three';
+import "./style.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+const Box = () => {
+    const [hovered, setHovered] = useState (false)
+    const [active, setActive] = useState (false)
+    const props = useSpring ({
+        
+        scale :active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+        color: hovered ? "hotpink" : "blue",
+    })
+                            
+    return (
+            <a.mesh
+            onPointerOver={() => setHovered(true)}
+            onPointerOut={() => setHovered(false)}
+            onClick={() => setActive(!active)}
+            scale={props.scale}
+            >
+    
+    <boxBufferGeometry attach="geometry" arg  s={[1, 1, 1]} />
+    <meshBasicMaterial attach="material" color={props.color} />
+</a.mesh>
+ )
 }
+
+
+
+export default () => (
+                      <Canvas>
+    <Box />
+                      </Canvas>
+)
